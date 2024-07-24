fd38e6482b6a30583fb6f88368a84b64b17f92f1
Andrea-Asaro <andrea.asaro.24@gmail.com>
2024-07-24 12:14:53 +0200

    reso mappa fully responsive con funzioni

--- src/components/ContactsMap.jsx
+++ src/components/ContactsMap.jsx
@@ -1,6 +1,36 @@
 import "./ContactsMap.css"
+import React, { useState, useEffect } from "react";
 
 export default function ContactsMap(){
+
+    const [dimensions, setDimensions] = useState(getIframeDimensions());
+
+    useEffect(() => {
+        const handleResize = () => {
+            setDimensions(getIframeDimensions());
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    // handleresize viene chiamata ogni volta la finestra cambia di width, e nel caso setta le dimensions
+
+    function getIframeDimensions() {
+        const width = window.innerWidth;
+        if (width < 576) {
+            return { width: "400", height: "200" };
+        } else if (width < 767) {
+            return { width: "450", height: "300" };
+        } else if (width < 992) {
+            return { width: "690", height: "400" };
+        } else {
+            return { width: "855", height: "600" };
+        }
+    }
+    // vede la width della pagina e in base a quella setta le dimensioni desiderate per la mappa
 
     return(
         <>
@@ -24,7 +54,7 @@ export default function ContactsMap(){
                 <div className="col-lg-8">
                     <h3 className="mb-4 text1 mt-3 mt-lg-0 fw-bold">DOVE SIAMO</h3>
                     <div className="pe-lg-5 mapResponsive">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d748.4938900326804!2d16.15151871454879!3d41.37461570703148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13382025fbb79855%3A0xb7749c4547df0136!2sVia%20Principe%20Amedeo%2C%201%2C%2076016%20Margherita%20di%20Savoia%20BT!5e0!3m2!1sen!2sit!4v1721811894757!5m2!1sen!2sit" width="856" height="600"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d748.4938900326804!2d16.15151871454879!3d41.37461570703148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13382025fbb79855%3A0xb7749c4547df0136!2sVia%20Principe%20Amedeo%2C%201%2C%2076016%20Margherita%20di%20Savoia%20BT!5e0!3m2!1sen!2sit!4v1721811894757!5m2!1sen!2sit" width={dimensions.width} height={dimensions.height}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
