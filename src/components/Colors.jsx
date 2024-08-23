a810db8ffaaa495e77007650f1fc718e30ca25c8
Andrea-Asaro <andrea.asaro.24@gmail.com>
2024-08-23 12:33:35 +0200

    completato modale per dettagli stanza, con carosello per immagini, e testo sotto per descrizione dettagliata della stanza

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -1,3 +1,14 @@
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 import Card from './Card';
 
 // import { useNavigate } from 'react-router-dom'; 
@@ -90,7 +101,46 @@ export default function Colors(){
                     </div>
 
                     <div className="modal-body bg0 border-0 font2 text3">
-                        Contenuto della modale
+
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-12 p-3 swiperCol">
+                                    <Swiper
+                                        slidesPerView={'auto'}
+                                        spaceBetween={30}
+                                        pagination={{
+                                        clickable: true,
+                                        }}
+                                        grabCursor={true}
+                                        modules={[Pagination]}
+                                        className="mySwiper"
+                                    >
+                                        <SwiperSlide><img src={riccio} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={luminaire} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={verde} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={riccio} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={luminaire} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={verde} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={luminaire} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={verde} alt="" /></SwiperSlide>
+                                        <SwiperSlide><img src={riccio} alt="" /></SwiperSlide>
+                                    </Swiper>
+
+                                </div>
+                                <div className="col-12 mt-lg-3">
+                                    <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolores culpa quo facilis nulla ipsa, nisi dolorum laborum, eveniet vitae placeat incidunt sequi distinctio quod atque sunt minima dignissimos maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat blanditiis ullam atque minus vero natus explicabo, nisi soluta veniam aspernatur officia animi similique laudantium quos, neque eius molestias eos recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ratione, accusantium qui dolores. Nemo aliquid tempore dolorem.
+                                    </p>
+                                    <ul id='detailsBulletList'>
+                                        <li>Bullet point n.1</li>
+                                        <li>Bullet point n.2</li>
+                                        <li>Bullet point n.3</li>
+                                        <li>Bullet point n.4</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
                     <div className="modal-footer bg0 border-0">
@@ -99,7 +149,7 @@ export default function Colors(){
                             <a target='blank' href="https://www.booking.com/hotel/it/copacabana-colors-margherita-di-savoia.it.html?aid=311091&label=copacabana-colors-margherita-di-savoia-Ket%2AnM2r9rdqJk5m3MbsNwS450167591004%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666995%3Akwd-400031164451%3Alp1008080%3Ali%3Adec%3Adm&sid=21b28a810709e5d6da1613a5f4b56075&dest_id=-121072;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1724249448;srpvid=8a3063afcf5201be;type=total;ucfs=1&">Prenota</a>
                         </button>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
